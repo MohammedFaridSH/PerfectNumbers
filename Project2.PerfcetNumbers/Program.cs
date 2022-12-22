5449b37a0de7b894d69120219de2f786615d284f
MohammedFaridSH <112472062+MohammedFaridSH@users.noreply.github.com>
2022-12-22 11:03:26 +0200

    Add files via upload

---
+++ Project2.PerfcetNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+public class PerfectN
+{
+    public static void Main()
+    {
+        int Output, x, y;
+        int LowEnd, HighEnd;
+
+        Console.Write("Input the starting range or number : ");
+        LowEnd = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input the ending range of number : ");
+        HighEnd = Convert.ToInt32(Console.ReadLine());
+        Console.Write("The Perfect numbers within the given range : ");
+        for (Output = LowEnd; Output <= HighEnd; Output++)
+        {
+            x = 1;
+            y = 0;
+            while (x < Output)
+            {
+                if (Output % x == 0)
+                 y = y + x;
+                x++;
+            }
+            if (y == Output)
+                Console.Write("{0} ", Output);
+        }
+        
+    }
+}
